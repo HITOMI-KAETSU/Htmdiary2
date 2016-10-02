0d2f1e1670c4e3bb646cb18731ee575df8562beb
HITOMI <xxxhimikxxx@hotmail.co.jp>
2016-10-02 03:32:56 +0000

    いいね機能の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
+  #ログインページに強制的にいっくようにする↑
   # GET /articles
   # GET /articles.json
   def index
